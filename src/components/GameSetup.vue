d4b0fa0534d8802f46cf96efeb30836e3e21be12
buxxi <buxxi@omfilm.net>
2024-10-07 17:23:49 +0200

    Difficulty selector and styling the setup popup

--- src/components/GameSetup.vue
+++ src/components/GameSetup.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type Result from '@/domain/result.ts';
 import Game, { GameMode, type Language } from '../services/game.ts';
+import { Difficulty, getDifficultySetting } from '@/domain/difficulty.ts';
+
 
 const emit = defineEmits<{
   (e: 'started', game: Game, result: Result): void
@@ -8,13 +10,13 @@ const emit = defineEmits<{
 
 const language = defineModel<string>('language', { default: 'en'});
 const type = defineModel<string>('type', { default : 'curated'});
-const articles = defineModel<number>('articles', { default : 2});
-const bombs = defineModel<number>('bombs', { default : 1});
+const difficulty = defineModel<number>('difficulty', { default : 0});
 
 async function startGame() {
   let game = new Game();
   let gameMode = type.value as GameMode;
-  let result = await game.start(language.value as Language, articles.value, bombs.value, gameMode);
+  let selectedDifficulty = getDifficultySetting(difficulty.value);
+  let result = await game.start(language.value as Language, selectedDifficulty.articles, selectedDifficulty.bombs, gameMode);
   emit('started', game, result);
 }
 </script>
@@ -22,41 +24,45 @@ async function startGame() {
 
 <template>
   <dialog open>
-    <h1>Wiki connector {{ language }}</h1>
     <form>
-      <fieldset>
-        <label for="language">Language</label>
-        <select id="language" v-model="language">
-          <option value="en">English</option>
-          <option value="sv">Swedish</option>
-        </select>
-      </fieldset>
-      <fieldset>
-        <label for="type">Type</label>
-        <select id="type" v-model="type">
-          <option value="curated">Curated</option>
-          <option value="random">Random</option>
-          <option value="popular">Popular</option>
-        </select>
-      </fieldset>
-      <fieldset>
-        <label for="articles">Articles</label>
-        <select id="articles" v-model.number="articles">
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-      </fieldset>
-      <fieldset>
-        <label for="bombs">Bombs</label>
-        <select id="bombs" v-model.number="bombs">
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-      </fieldset>
+      <h1>Wiki connector</h1>
+      <div>
+        <section class="settings">
+          <h2>Settings</h2>
+          <fieldset>
+            <label for="language">Language</label>
+            <select id="language" v-model="language">
+              <option value="en">🇺🇸 English</option>
+              <option value="sv">🇸🇪 Swedish</option>
+            </select>
+          </fieldset>
+          <fieldset>
+            <label for="type">Type</label>
+            <select id="type" v-model="type">
+              <option value="curated">Curated</option>
+              <option value="random">Random</option>
+              <option value="popular">Popular</option>
+            </select>
+          </fieldset>
+          <fieldset>
+            <label for="difficulty">Difficulty</label>
+            <input id="difficulty" type="range" list="difficulties" min="0" max="4" v-model.number="difficulty"/>
+            <span class="difficulty-smiley">{{ getDifficultySetting(difficulty).smiley }}</span>
+            
+            <datalist id="difficulties">
+              <option v-for="([value, key], idx) in Object.entries(Difficulty).filter(([_, k]) => Number.isInteger(k))" :value="key">{{ value }}</option>
+            </datalist>
+          </fieldset>
+        </section>
+        <section class="rules">
+          <h2>How to play</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a rhoncus ex. <b>{{  getDifficultySetting(difficulty).articles }}</b><br/>
+            Cras venenatis efficitur nulla quis consequat <b>{{  getDifficultySetting(difficulty).bombs }}</b>. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.<br/>
+            Fusce hendrerit tempus nisi, in congue urna. Sed pretium magna ac ex semper pharetra. Pellentesque sit amet rutrum metus. 
+          </p>
+        </section>
+      </div>
       <input type="button" value="Start" @click.prevent="startGame"/>
     </form>
   </dialog>
