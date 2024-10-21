9e413f7776d8c5a9622f51f29349989ab47eadf5
buxxi <buxxi@omfilm.net>
2024-10-21 18:01:28 +0200

    extracting common code for jigsaw pieces and use Path2D for better caching

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -2,6 +2,8 @@
 	import { onMounted, watch } from "vue";
 
 	const MAX_FPS = 30;
+	const JIGSAW_PIECE_SIZE = 50;
+	const JIGSAW_HOLE_SIZE = 10;
 
 	const props = defineProps<{
 		animate: boolean
@@ -10,92 +12,103 @@
 	class JigsawPiece {
 		x: number;
 		y: number;
-		size: number;
-		holeSize: number;
 		up: boolean;
 		right: boolean;
 		bottom: boolean;
 		left: boolean;
 		hue: number;
+		path: Path2D;
 
-		constructor(x: number, y: number, size: number, holeSize: number, up: boolean, right: boolean, bottom: boolean, left: boolean, hue: number) {
+		constructor(x: number, y: number, up: boolean, right: boolean, bottom: boolean, left: boolean, hue: number) {
 			this.x = x;
 			this.y = y;
-			this.size = size;
-			this.holeSize = holeSize;
 			this.up = up;
 			this.right = right;
 			this.bottom = bottom;
 			this.left = left;
 			this.hue = hue;
+			this.path = this._generatePath();
 		}
 
 		draw(context: CanvasRenderingContext2D, lightness: number) {
-			context.beginPath();
-			context.moveTo(this.x, this.y);
-			this._drawUpper(context, this.x, this.y, this.up ? 1 : -1);
-			this._drawRight(context, this.x + this.size, this.y, this.right ? 1 : -1);
-			this._drawBottom(context, this.x + this.size, this.y + this.size, this.bottom ? 1 : -1);
-			this._drawLeft(context, this.x, this.y + this.size, this.left ? 1 : -1);
-
 			context.fillStyle = `hsl(${this.hue}, 10%, ${15 + (lightness * 10)}%)`;
-			context.fill();
+			context.fill(this.path);
 			context.strokeStyle = `hsl(${this.hue}, 0%, 10%)`;
-			context.stroke();
+			context.stroke(this.path);
 		}
 
-		_drawUpper(context: CanvasRenderingContext2D, fromX: number, fromY: number, modY: number) {
-			let centerX = fromX + (this.size / 2);
-			let halfHoleWidth = this.holeSize / 2;
+		_generatePath(): Path2D {
+			let path = new Path2D();
+			path.moveTo(this.x, this.y);
+			this._drawUpper(path, this.x, this.y, this.up ? 1 : -1);
+			this._drawRight(path, this.x + JIGSAW_PIECE_SIZE, this.y, this.right ? 1 : -1);
+			this._drawBottom(path, this.x + JIGSAW_PIECE_SIZE, this.y + JIGSAW_PIECE_SIZE, this.bottom ? 1 : -1);
+			this._drawLeft(path, this.x, this.y + JIGSAW_PIECE_SIZE, this.left ? 1 : -1);
+			return path;
+		}
+
+		_drawUpper(context: CanvasPath, fromX: number, fromY: number, modY: number) {
+			let centerX = fromX + (JIGSAW_PIECE_SIZE / 2);
+			const { halfHoleWidth, beforeHoleIndent, cp1Indent, cp2Indent } = this._sizes(modY);
+
 			context.lineTo(centerX - halfHoleWidth, fromY);
-			context.lineTo(centerX - halfHoleWidth, fromY - (0.3 * this.holeSize * modY));
-			context.bezierCurveTo(centerX - this.holeSize, fromY - (0.8 * this.holeSize * modY), centerX - this.holeSize, fromY - (1.3 * this.holeSize * modY), centerX, fromY - (1.3 * this.holeSize * modY));
-			context.bezierCurveTo(centerX + this.holeSize, fromY - (1.3 * this.holeSize * modY), centerX + this.holeSize, fromY - (0.8 * this.holeSize * modY), centerX + halfHoleWidth, fromY - (0.3 * this.holeSize * modY));
+			context.lineTo(centerX - halfHoleWidth, fromY - beforeHoleIndent);
+			context.bezierCurveTo(centerX - JIGSAW_HOLE_SIZE, fromY - cp1Indent, centerX - JIGSAW_HOLE_SIZE, fromY - cp2Indent, centerX, fromY - cp2Indent);
+			context.bezierCurveTo(centerX + JIGSAW_HOLE_SIZE, fromY - cp2Indent, centerX + JIGSAW_HOLE_SIZE, fromY - cp1Indent, centerX + halfHoleWidth, fromY - beforeHoleIndent);
 			context.lineTo(centerX + halfHoleWidth, fromY);
-			context.lineTo(fromX + this.size, fromY);
+			context.lineTo(fromX + JIGSAW_PIECE_SIZE, fromY);
 		}
 
-		_drawBottom(context: CanvasRenderingContext2D, fromX: number, fromY: number, modY: number) {
-			let centerX = fromX - (this.size / 2);
-			let halfHoleWidth = this.holeSize / 2;
+		_drawBottom(context: CanvasPath, fromX: number, fromY: number, modY: number) {
+			let centerX = fromX - (JIGSAW_PIECE_SIZE / 2);
+			const { halfHoleWidth, beforeHoleIndent, cp1Indent, cp2Indent } = this._sizes(modY);
+
 			context.lineTo(centerX + halfHoleWidth, fromY);
-			context.lineTo(centerX + halfHoleWidth, fromY + (0.3 * this.holeSize * modY));
-			context.bezierCurveTo(centerX + this.holeSize, fromY + (0.8 * this.holeSize * modY), centerX + this.holeSize, fromY + (1.3 * this.holeSize * modY), centerX, fromY + (1.3 * this.holeSize * modY));
-			context.bezierCurveTo(centerX - this.holeSize, fromY + (1.3 * this.holeSize * modY), centerX - this.holeSize, fromY + (0.8 * this.holeSize * modY), centerX - halfHoleWidth, fromY + (0.3 * this.holeSize * modY));
+			context.lineTo(centerX + halfHoleWidth, fromY + beforeHoleIndent);
+			context.bezierCurveTo(centerX + JIGSAW_HOLE_SIZE, fromY + cp1Indent, centerX + JIGSAW_HOLE_SIZE, fromY + cp2Indent, centerX, fromY + cp2Indent);
+			context.bezierCurveTo(centerX - JIGSAW_HOLE_SIZE, fromY + cp2Indent, centerX - JIGSAW_HOLE_SIZE, fromY + cp1Indent, centerX - halfHoleWidth, fromY + beforeHoleIndent);
 			context.lineTo(centerX - halfHoleWidth, fromY);
-			context.lineTo(fromX - this.size, fromY);
+			context.lineTo(fromX - JIGSAW_PIECE_SIZE, fromY);
 		}
 
-		_drawRight(context: CanvasRenderingContext2D, fromX: number, fromY: number, modX: number) {
-			let centerY = fromY + (this.size / 2);
-			let halfHoleWidth = this.holeSize / 2;
+		_drawRight(context: CanvasPath, fromX: number, fromY: number, modX: number) {
+			let centerY = fromY + (JIGSAW_PIECE_SIZE / 2);
+			const { halfHoleWidth, beforeHoleIndent, cp1Indent, cp2Indent } = this._sizes(modX);
+
 			context.lineTo(fromX, centerY - halfHoleWidth);
-			context.lineTo(fromX + (0.3 * this.holeSize * modX), centerY - halfHoleWidth);
-			context.bezierCurveTo(fromX + (0.8 * this.holeSize * modX), centerY - this.holeSize, fromX + (1.3 * this.holeSize * modX), centerY - this.holeSize, fromX + (1.3 * this.holeSize * modX), centerY);
-			context.bezierCurveTo(fromX + (1.3 * this.holeSize * modX), centerY + this.holeSize, fromX + (0.8 * this.holeSize * modX), centerY + this.holeSize, fromX + (0.3 * this.holeSize * modX), centerY + halfHoleWidth);
+			context.lineTo(fromX + beforeHoleIndent, centerY - halfHoleWidth);
+			context.bezierCurveTo(fromX + cp1Indent, centerY - JIGSAW_HOLE_SIZE, fromX + cp2Indent, centerY - JIGSAW_HOLE_SIZE, fromX + cp2Indent, centerY);
+			context.bezierCurveTo(fromX + cp2Indent, centerY + JIGSAW_HOLE_SIZE, fromX + cp1Indent, centerY + JIGSAW_HOLE_SIZE, fromX + beforeHoleIndent, centerY + halfHoleWidth);
 			context.lineTo(fromX, centerY + halfHoleWidth);
-			context.lineTo(fromX, fromY + this.size);
+			context.lineTo(fromX, fromY + JIGSAW_PIECE_SIZE);
 		}
 
-		_drawLeft(context: CanvasRenderingContext2D, fromX: number, fromY: number, modX: number) {
-			let centerY = fromY - (this.size / 2);
-			let halfHoleWidth = this.holeSize / 2;
+		_drawLeft(context: CanvasPath, fromX: number, fromY: number, modX: number) {
+			let centerY = fromY - (JIGSAW_PIECE_SIZE / 2);
+			const { halfHoleWidth, beforeHoleIndent, cp1Indent, cp2Indent } = this._sizes(modX);
 			context.lineTo(fromX, centerY + halfHoleWidth);
-			context.lineTo(fromX - (0.3 * this.holeSize * modX), centerY + halfHoleWidth);
-			context.bezierCurveTo(fromX - (0.8 * this.holeSize * modX), centerY + this.holeSize, fromX - (1.3 * this.holeSize * modX), centerY + this.holeSize, fromX - (1.3 * this.holeSize * modX), centerY);
-			context.bezierCurveTo(fromX - (1.3 * this.holeSize * modX), centerY - this.holeSize, fromX - (0.8 * this.holeSize * modX), centerY - this.holeSize, fromX - (0.3 * this.holeSize * modX), centerY - halfHoleWidth);
+			context.lineTo(fromX - beforeHoleIndent, centerY + halfHoleWidth);
+			context.bezierCurveTo(fromX - cp1Indent, centerY + JIGSAW_HOLE_SIZE, fromX - cp2Indent, centerY + JIGSAW_HOLE_SIZE, fromX - cp2Indent, centerY);
+			context.bezierCurveTo(fromX - cp2Indent, centerY - JIGSAW_HOLE_SIZE, fromX - cp1Indent, centerY - JIGSAW_HOLE_SIZE, fromX - beforeHoleIndent, centerY - halfHoleWidth);
 			context.lineTo(fromX, centerY - halfHoleWidth);
-			context.lineTo(fromX, fromY - this.size);
+			context.lineTo(fromX, fromY - JIGSAW_PIECE_SIZE);
+		}
+
+		_sizes(mod: number) {
+			return {
+				halfHoleWidth: JIGSAW_HOLE_SIZE / 2,
+				beforeHoleIndent: (0.3 * JIGSAW_HOLE_SIZE * mod),
+				cp1Indent: (0.8 * JIGSAW_HOLE_SIZE * mod),
+				cp2Indent: (1.3 * JIGSAW_HOLE_SIZE * mod)
+			}
 		}
 	}
 
 
 
 	function generatePieces(width: number, height: number): JigsawPiece[][] {
-		let size = 50;
-		let holeSize = 10;
-		let rows = Math.ceil(height / size);
-		let cols = Math.ceil(width / size);
+		let rows = Math.ceil(height / JIGSAW_PIECE_SIZE);
+		let cols = Math.ceil(width / JIGSAW_PIECE_SIZE);
 		let result: JigsawPiece[][] = [];
 		var currentRow: JigsawPiece[] = [];
 		for (var y = 0; y < rows; y++) {
@@ -106,7 +119,7 @@
 				let bottom = Math.random() < 0.5;
 				let left = x == 0 || !result[y][x - 1].right;
 				let hue = Math.floor(Math.random() * 360);
-				currentRow.push(new JigsawPiece(x * size, y * size, size, holeSize, up, right, bottom, left, hue));
+				currentRow.push(new JigsawPiece(x * JIGSAW_PIECE_SIZE, y * JIGSAW_PIECE_SIZE, up, right, bottom, left, hue));
 			}
 			currentRow = [];
 		}
